db42aaef0f9e6d3994c91162f655290234bbd239
岩熊 <yanxiong@lap81-h01-176-130-2-4.dsl.sta.abo.bbox.fr>
2016-12-19 15:12:44 +0800

    new create a class

---
+++ test/TestClass.h
@@ -0,0 +1,13 @@
+//
+//  TestClass.h
+//  test
+//
+//  Created by 岩  熊 on 2016/12/19.
+//  Copyright © 2016年 xiongyan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TestClass : NSObject
+
+@end
---
+++ test/TestClass.m
@@ -0,0 +1,13 @@
+//
+//  TestClass.m
+//  test
+//
+//  Created by 岩  熊 on 2016/12/19.
+//  Copyright © 2016年 xiongyan. All rights reserved.
+//
+
+#import "TestClass.h"
+
+@implementation TestClass
+
+@end
